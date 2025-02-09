4bd0f83824ae6cd36bb37c7bb9eabd209ab5ec7e
kmw <skqjahjakskxl@gmail.com>
2025-02-10 01:34:55 +0900

    서버 배포 오류 수정 3

--- backend/urls.py
+++ backend/urls.py
@@ -2,24 +2,19 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 def index(request):
     return HttpResponse("Hello, this is the homepage of the API backend.")
 
 urlpatterns = [
-    path('', index),  # 루트 URL에 간단한 뷰 추가
+    path('', index),  # 기본 루트 URL에 대한 뷰 추가
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
     path('api/color/', include('color.urls')),
     path('api/community/', include('community.urls')),
     path('api/recommendation/', include('recommendation.urls')),
     path('api/custom_palette/', include('custom_palette.urls')),
-    
-    # JWT 관련 엔드포인트
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
